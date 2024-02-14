a1dfe286f7fdecad23a21900d0f0691cb91f7fb5
Krishna-Duddumpudi <149926378+Krishna-Duddumpudi@users.noreply.github.com>
2024-02-14 15:18:30 +1300

    init

---
+++ src/index.js
@@ -0,0 +1,40 @@
+
+import globeDiv from "./globe.js";
+import heroDiv from "./hero.js";
+
+/*
+
+
+let heroAniDiv = document.getElementById("vectorField");
+export let heroClientWidth = heroAniDiv.clientWidth;
+export let heroClientHeight = heroAniDiv.clientHeight;
+
+
+let globeAniDiv = document.getElementById("globeAni");
+export let globeClientWidth = globeAniDiv.clientWidth;
+export let globeClientHeight = globeAniDiv.clientHeight;
+
+new heroDiv("vectorField");
+new globeDiv("globeAni");
+
+export function deviceCheck(onMobile) {
+  if (/Android|iPhone/i.test(navigator.userAgent)) {
+    // mobile
+    onMobile = true;
+    console.log("user is on mobile");
+
+  } else {
+    // desktop
+    onMobile = false;
+    console.log("user is on desktop");
+  }
+  return onMobile;
+}
+
+/*
+export function preLoadFonts(neue, helvetica) {
+  //load fonts
+  neue = p5.loadFont('https://cdn.jsdelivr.net/gh/Krishna-Duddumpudi/threejs_cdn_test/NeuePixel-Regular.otf');
+  helvetica = p5.loadFont('https://cdn.jsdelivr.net/gh/Krishna-Duddumpudi/threejs_cdn_test/HelveticaNeueLTStd-Roman.otf');
+}
+*/
